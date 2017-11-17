f013c8057ae7b3c6e13f9a2233c8f2f0f91c73e2
Daniel Moeller <daniel.moeller@oddeven.ch>
2017-11-17 15:33:24 +0100

    Added Work Unit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `<p>{{message}}</p>`
+    selector: 'app-root',
+    template: `
+    <input type="text"
+           [value]="value"
+           [attr.aria-invalid]="error"
+           [class.is-valid]="!error"
+           (input)="onInput($event.target.value)">
+    <p [innerHTML]="message"
+       [style.color]="error ? 'red' : 'green'"></p>
+  `
 })
 export class AppComponent {
-  message: string  = 'Hello World!';
+    value: string = '';
+    message: string = 'All is fine...';
+    error: boolean = false;
+
+    onInput(value) {
+        this.value = value;
+        if (this.value.length > 10) {
+            this.message = 'Value is too long';
+            this.error = true;
+        } else {
+            this.message = 'All is fine...';
+            this.error = false;
+        }
+    }
 }
