9977ba086b6705ba28e24633f836e67349a77234
Daniel Moeller <djm@scandinavianapps.com>
2017-11-24 14:17:26 +0100

    Added Work Unit 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<p>{{message}}</p>`
+  template: `
+    <collapsible [title]="title">
+      <p>I'm regular content that will be inserted into the collapsible body</p>
+    </collapsible>
+  `
 })
 export class AppComponent {
-  message: string  = 'Hello World!';
+  title: string = 'Simple collapsible';
 }
---
+++ src/app/collapsible-component.ts
@@ -0,0 +1,19 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'collapsible',
+  template: `
+    <h3 (click)="toggle()">{{title}}</h3>
+    <div [style.display]="open ? 'block' : 'none'">
+      <ng-content></ng-content>
+    </div>
+  `
+})
+export class CollapsibleComponent {
+  @Input() title: string;
+  open: boolean = false;
+
+  toggle() {
+    this.open = !this.open;
+  }
+}
