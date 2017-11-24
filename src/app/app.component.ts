b5b470d3b90bea7a9768d90824b87ffafb02f1fa
Daniel Moeller <djm@scandinavianapps.com>
2017-11-24 14:13:58 +0100

    Added Work Unit 3 progressed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,23 @@ import {Component} from '@angular/core';
 import {CollapsibleComponent} from './collapsible-component';
 
 @Component({
-    selector: 'app-root',
-    template: `
-        <collapsible [content]="content" (opened)="onOpened($event)"></collapsible>
-    `
+  selector: 'app-root',
+  template: `
+    <collapsible [title]="title"
+                 [content]="content"
+                 (opened)="onOpened($event)"
+                 (closed)="onClosed($event)"></collapsible>
+  `
 })
 export class AppComponent {
-    content: string = 'This is a simple collapsible content';
+  title: string = 'Simple collapsible';
+  content: string = 'This is a simple collapsible content';
 
-    onOpened(collapsible: CollapsibleComponent) {
-        console.log('Collapsible was opened', collapsible);
-    }
+  onOpened(collapsible: CollapsibleComponent) {
+    console.log('Collapsible was opened', collapsible);
+  }
+
+  onClosed(collapsible: CollapsibleComponent) {
+    console.log('Collapsible was closed', collapsible);
+  }
 }
--- src/app/collapsible-component.ts
+++ src/app/collapsible-component.ts
@@ -3,15 +3,17 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
 @Component({
   selector: 'collapsible',
   template: `
-    <h3 (click)="toggle()">Collapsible click to open</h3>
+    <h3 (click)="toggle()">{{title}}</h3>
     <div [style.display]="open ? 'block' : 'none'">
       {{content}}
     </div>
   `
 })
 export class CollapsibleComponent {
+  @Input() title: string;
   @Input() content: string;
   @Output() opened: EventEmitter<CollapsibleComponent> = new EventEmitter();
+  @Output() closed: EventEmitter<CollapsibleComponent> = new EventEmitter();
   open: boolean = false;
 
   toggle() {
@@ -19,6 +21,8 @@ export class CollapsibleComponent {
 
     if (this.open) {
       this.opened.emit(this);
+    } else {
+      this.closed.emit(this);
     }
   }
 }
