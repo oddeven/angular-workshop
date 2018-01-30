6c3b9dadb1817db5323c91081c6b79044f1c3802
Daniel Moeller <djm@scandinavianapps.com>
2018-01-30 12:41:46 +0100

    Unit Test Service Prepared

---
+++ src/app/calculation.service.spec.ts
@@ -0,0 +1,14 @@
+import { CalculationService } from './calculation.service';
+
+describe('CalculationService', () => {
+  let service: CalculationService = new CalculationService();
+
+  it('service instance exists', () => {
+    expect(service).toBeTruthy();
+  });
+
+  // TODO: Test service.add
+  // TODO: Test service.subtract
+  // TODO: Test service.multiply
+  // TODO: Test service.divide
+});
