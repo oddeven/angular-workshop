d1023e3809a286e6a5657f6eb9c6ef7e81c5d092
Daniel Moeller <djm@scandinavianapps.com>
2018-01-31 17:39:24 +0100

    Refactored

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,30 +7,26 @@ import {Component} from '@angular/core';
                [value]="value"
                [attr.aria-invalid]="error"
                [class.is-valid]="!error"
-               (input)="onInput($event.target.value)">
+               (input)="onInput($event.target.value)" />
         <button (click)="reset()">Reset</button>
         <p [innerHTML]="message"
-           [style.display]="error ? 'block' : 'none'"></p>
+           [style.color]="'red'" 
+           [style.display]="error ? 'block' : 'none'">
+        </p>
     `
 })
 export class AppComponent {
     value: string = '';
-    message: string = 'All is fine...';
+    message: string = 'Value is too long';
     error: boolean = false;
 
     onInput(value) {
         this.value = value;
-        if (this.value.length > 10) {
-            this.message = 'Value is too long';
-            this.error = true;
-        } else {
-            this.error = false;
-        }
+        this.error = this.value.length > 10;
     }
 
     reset() {
-        this.error = false;
         this.value = '';
-        this.message = '';
+        this.error = false;
     }
 }
